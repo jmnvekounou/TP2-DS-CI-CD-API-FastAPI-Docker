ef3adb36a4c9148318d4487dab93caaff58e6ccf
Moise <jmnvekounou@gmail.com>
2026-08-29 17:21:43 -0400

    adding .github/workflows/ci.yml

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -16,15 +16,44 @@ def test_favicon():
 
 def test_predict_success():
     """
-        Cette fonction test et valide une prédiction correcte avec les valeur: [1.0, 2.0, 3.0] 
+        Cette fonction test et valide une prédiction correcte avec les valeur: [1.0, 2.0, 3.0]
     """
     payload = {"features": [1.0, 2.0, 3.0]}
-    
+
     # Envoi de la requête
     response = client.post("/predict", json = payload)
-    
+
     # Validation des assertions
     assert response.status_code == 200
 
     assert  response.json() == {"predictions": [2.0, 4.0, 6.0]}
 
+@patch("app.main.predict")
+def test_predict_incorrect(mock_predict):
+    """
+        Cette fonction test et valide valide une prédiction incorrecte 
+    """
+    # Configuration de la fausse vrai prédiction
+    mock_predict.return_value = [0.5, 0.8]
+    payload = {"features": [1.0, 2.0, 3.0]}
+    
+    # Exécution de la requête
+    response = client.post("/predict", json=payload)
+    
+    # Validation des assertions
+    assert response.status_code == 200
+        
+    assert response.json() != {"predictions": [0.7, 0.95]}
+
+
+def test_predict_invalid_data():
+    """
+        Cette fonction test et valide une prédiction incorrect en envoyant un JSON incorrect
+    """
+    # Invalide payload
+    payload = {"invalid_payload":[3.5, 1.2, 4.9]}
+
+    # Envoi de la requête
+    response = client.post("/predict", json = payload)
+
+    assert response.status_code == 422
